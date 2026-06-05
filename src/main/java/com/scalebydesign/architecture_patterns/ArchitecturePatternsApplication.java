a554092276362f22b27bc038a29de87e1935b35f
Abhishek Bansal <abhishek.bansal@sbs-software.com>
2026-06-05 14:04:58 +0530

    initial commit

---
+++ src/main/java/com/scalebydesign/architecture_patterns/ArchitecturePatternsApplication.java
@@ -0,0 +1,13 @@
+package com.scalebydesign.architecture_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturePatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitecturePatternsApplication.class, args);
+	}
+
+}
